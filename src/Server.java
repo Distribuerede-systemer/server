c00511a3f684cfecff234cb23bc4e9eb50287236
RasmusBM <rasmus_bm@hotmail.com>
2014-09-29 12:40:59 +0200

    Small change
    Small change

--- src/Server.java
+++ src/Server.java
@@ -2,6 +2,6 @@
 public class Server {
 
 	/*
-	I kan hente dette ned for at se om alt fungere. I kan evt. indskrive en ny kommentar her for at fors¿ge.
+	I kan hente dette ned for at se om alt fungere. I kan evt. indskrive en ny kommentar her for at forsï¿½ge.
 	*/
 }
