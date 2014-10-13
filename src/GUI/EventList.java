9631cbdf13f673442090e2226a73371bbb282f3f
hognipetersen <hognipetersen@gmail.com>
2014-10-13 15:04:20 +0200

    EventList GUI

--- src/GUI/EventList.java
+++ src/GUI/EventList.java
@@ -1,5 +1,61 @@
 package GUI;
+import java.awt.Dimension;
+import javax.swing.JScrollPane;
 
-public class EventList {
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
 
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JScrollBar;
+import javax.swing.JTable;
+
+public class EventList extends JFrame {
+
+	private JPanel contentPane;
+	private JTable table;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					EventList frame = new EventList();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public EventList() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		setSize(new Dimension(320, 468));
+		
+		//JScrollPane
+		JScrollPane scrollPane = new JScrollPane();
+		scrollPane.setBounds(0, 0, 320, 468);
+		getContentPane().add(scrollPane);
+		
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JScrollBar scrollBar = new JScrollBar();
+		scrollBar.setBounds(305, 0, 15, 440);
+		contentPane.add(scrollBar);
+		
+		table = new JTable();
+		table.setBounds(0, 0, 305, 446);
+		contentPane.add(table);
+	}
 }
