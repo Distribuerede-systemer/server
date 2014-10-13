12add9f7b9ae0db2926f251207a1a46f5553122e
bjarkeds <bjarke.schioette@live.dk>
2014-10-13 17:29:32 +0200

    Model - Abstract

--- src/model/Model.java
+++ src/model/Model.java
@@ -1,13 +1,12 @@
 package model;
 
-
 import java.sql.*;
 
 /**
  * Model superclass, never instansiated. All child model classes inherits its properties, classes and methods
  * Created by jesperbruun on 13/10/14.
  */
-public  class Model {
+public abstract class Model {
 
     private static String sqlUrl = "jdbc:mysql://localhost:3306/";
     private static String sqlUser = "root";
