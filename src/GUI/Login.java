c5e3cf9dc04c6e7f33984fe1055c4fd51838a46b
Mathias Okholm Nielsen <maddi9@hotmail.com>
2014-10-13 11:25:03 +0200

    Res mappe oprettet

--- src/GUI/Login.java
+++ src/GUI/Login.java
@@ -5,18 +5,23 @@ import javax.swing.JLabel;
 import java.awt.Font;
 import javax.swing.JTextField;
 import javax.swing.ImageIcon;
+import javax.swing.JButton;
 
 public class Login extends JPanel {
 	private final JLabel lblWelcome = new JLabel("Welcome");
 	private final JLabel lblUsername = new JLabel("Username");
 	private final JTextField textField = new JTextField();
 	private final JLabel lblPassword = new JLabel("Password");
-	private final JLabel label = new JLabel("");
+	private final JTextField textField_1 = new JTextField();
+	private final JButton btnLogIn = new JButton("Log In");
+	private final JButton btnForgotLogIn = new JButton("Forgot log in?");
 
 	/**
 	 * Create the panel.
 	 */
 	public Login() {
+		textField_1.setBounds(174, 169, 86, 20);
+		textField_1.setColumns(10);
 		textField.setBounds(173, 116, 86, 20);
 		textField.setColumns(10);
 		setLayout(null);
@@ -32,10 +37,14 @@ public class Login extends JPanel {
 		lblPassword.setBounds(196, 148, 63, 14);
 		
 		add(lblPassword);
-		label.setIcon(new ImageIcon("C:\\Users\\Mathias\\Documents\\GitHub\\server\\src\\Images\\background.jpg"));
-		label.setBounds(0, 1, 455, 313);
 		
-		add(label);
+		add(textField_1);
+		btnLogIn.setBounds(175, 206, 89, 23);
+		
+		add(btnLogIn);
+		btnForgotLogIn.setBounds(173, 240, 97, 23);
+		
+		add(btnForgotLogIn);
 
 	}
 }
