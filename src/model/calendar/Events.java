236ff9bcac0463228d9d8dc04b3314e78c880478
Jesper Bruun <jeha13ad@student.cbs.dk>
2014-10-13 10:40:44 +0200

    Calendar API
    Get JSON string from URL and parse to Java Objects.
    Encrypt User ID added as well.

---
+++ src/model/calendar/Events.java
@@ -0,0 +1,24 @@
+package model.calendar;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+/**
+ * Created by jesperbruun on 10/10/14.
+ */
+public class Events {
+    ArrayList<Event> events = new ArrayList<Event>();
+
+    public ArrayList<Event> getEvents() {
+        return events;
+    }
+
+    public void setEvents(ArrayList<Event> event) {
+        this.events = event;
+    }
+
+    @Override
+    public String toString() {
+        return Arrays.toString(events.toArray());
+    }
+}
---
+++ src/model/calendar/GetCalendarData.java
@@ -0,0 +1,54 @@
+package model.calendar;
+
+import com.google.gson.Gson;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.URL;
+
+/**
+ * Created by jesperbruun on 13/10/14.
+ */
+public class GetCalendarData {
+
+
+    //Read URL
+    private static String readUrl(String urlString) throws Exception {
+        BufferedReader reader = null;
+        try {
+            URL url = new URL(urlString);
+            reader = new BufferedReader(new InputStreamReader(url.openStream()));
+            StringBuffer buffer = new StringBuffer();
+            int read;
+            char[] chars = new char[1024];
+            while ((read = reader.read(chars)) != -1)
+                buffer.append(chars, 0, read);
+
+            return buffer.toString();
+        } finally {
+            if (reader != null)
+                reader.close();
+        }
+
+    }
+
+
+    public void getDataFromCalendar() throws Exception {
+
+        /**
+         * Get URL From calendar.cbs.dk -> Subscribe -> change URL to end with .json
+         * Encrypt hash from
+         */
+        String json = readUrl("http://calendar.cbs.dk/events.php/jeha13ad/02a24d4e002e6e3571227c39e2f63784.json");
+
+        Gson gson = new Gson();
+        Events events = gson.fromJson(json, Events.class);
+
+        for (int i = 0; i < events.getEvents().size(); i++){
+            System.out.println(events.getEvents().get(i).getActivityid());
+        }
+
+
+    }
+
+}
