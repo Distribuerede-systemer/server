cde0c1a6897176b1ab4045b0c6ef6a341e84a7fb
Jákup Toftum <Jakuptj@gmail.com>
2014-10-13 16:24:37 +0200

    .

--- src/model/Notes.java
+++ src/model/Notes.java
@@ -12,7 +12,37 @@ public class Notes {
 		this.dateTime = dateTime;
 		
 	}
+
+	public int getNoteID() {
+		return noteID;
+	}
+
+	public void setNoteID(int noteID) {
+		this.noteID = noteID;
+	}
+
+	public String getText() {
+		return text;
+	}
+
+	public void setText(String text) {
+		this.text = text;
+	}
+
+	public String getDateTime() {
+		return dateTime;
+	}
+
+	public void setDateTime(String dateTime) {
+		this.dateTime = dateTime;
+	}
 	
+	public void saveNote () {
+		
+	}
 	
+	public void getNote () {
+		
+	}
 
 }
