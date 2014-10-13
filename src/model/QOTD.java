9c812c9b2dc44068439223716adbc105632636bf
Daniel Franch <franch@podio.com>
2014-10-13 14:26:21 +0200

    Changed to IP

--- src/model/QOTD.java
+++ src/model/QOTD.java
@@ -3,11 +3,11 @@ package model;
 public class QOTD {
 
     //Connect to server
-    private String server_url = "djxmmx.net";
+    private String server_ip = "104.139.50.121";
     private int server_port = 17;
 
     //Read content
-    
+
     //Gemme i database
 
 }
