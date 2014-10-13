7663a07debd01da6fbbe5be6e19aad0a14a84fd2
Daniel Franch <franch@podio.com>
2014-10-13 14:21:09 +0200

    Added some server information

--- src/model/QOTD.java
+++ src/model/QOTD.java
@@ -3,9 +3,11 @@ package model;
 public class QOTD {
 
     //Connect to server
+    private String server_url = "djxmmx.net";
+    private int server_port = 17;
 
     //Read content
-
+    
     //Gemme i database
 
 }
