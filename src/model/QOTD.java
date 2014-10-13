c25d8072d975ee1171cf0c53ffbe093365696f2c
Daniel Franch <franch@podio.com>
2014-10-13 14:41:53 +0200

    First attempt

--- src/model/QOTD.java
+++ src/model/QOTD.java
@@ -1,10 +1,12 @@
 package model;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
+import java.net.SocketException;
 
 
 public class QOTD {
@@ -13,27 +15,27 @@ public class QOTD {
     private String server_ip = "104.139.50.121";
     private int server_port = 17;
     //Read content
-    
-    public static void main (String[] args) throws Exception
-	{
-    
-    BufferedReader inFromUser = 
-			new BufferedReader ( new InputStreamReader(System.in));
-	DatagramSocket clientSocket = new DatagramSocket();
-	InetAddress IPAddress = InetAddress.getByName("localhost");
-	byte [] sendData = new byte [1024];
-	byte[] receiveData = new byte [1024];
-	String sentence = inFromUser.readLine();
-	sendData = sentence.getBytes();
-	
-	DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, IPAddress, 7000);
-	clientSocket.send(sendPacket);
-	DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
-	clientSocket.receive(receivePacket);
-	String modifiedSentence = new String (receivePacket.getData());
-	System.out.println("FROM SERVER:"+ modifiedSentence);
-	clientSocket.close();
 
+    public QOTD() {
+
+    }
+
+    public static void main(String args[]){
+    // args give message contents and server hostname
+        DatagramSocket aSocket = null;
+        try {
+            aSocket = new DatagramSocket();
+            byte [] m = new byte[1024];
+            InetAddress aHost = InetAddress.getByName("104.139.50.121");
+            int serverPort = 17;
+            DatagramPacket request = new DatagramPacket(m, m.length, aHost, serverPort);
+            aSocket.send(request);
+            byte[] buffer = new byte[1024];
+            DatagramPacket reply = new DatagramPacket(buffer, buffer.length);
+            aSocket.receive(reply);
+            System.out.println("Reply: " + new String(reply.getData()));
+        } catch (SocketException e){System.out.println("Socket: " + e.getMessage()); } catch (IOException e){System.out.println("IO: " + e.getMessage());
+        } finally { if(aSocket != null) aSocket.close();}
     }
     //Gemme i database
 }
