f65dbb736a453aebefa78ebbd4f9721d4f587c99
Jespermorch <Jespermorch@gmail.com>
2014-10-13 13:21:36 +0200

    NotesList

---
+++ src/GUI/NotesListGUI.java
@@ -0,0 +1,60 @@
+package GUI;
+
+import java.awt.BorderLayout;
+import java.awt.Dimension;
+import java.awt.EventQueue;
+
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JScrollBar;
+import javax.swing.JTable;
+
+import java.awt.event.MouseWheelListener;
+import java.awt.event.MouseWheelEvent;
+
+public class NotesListGUI extends JFrame {
+
+	private JPanel contentPane;
+	private JTable table;
+	private JScrollBar scrollBar;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					NotesListGUI frame = new NotesListGUI();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public NotesListGUI() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 330, 483);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(0, 0, 0, 0));
+		setContentPane(contentPane);
+		setSize(new Dimension(320, 468));
+		getContentPane().setLayout(null);
+		contentPane.setLayout(new BorderLayout(0, 0));
+		
+		table = new JTable();
+		contentPane.add(table, BorderLayout.CENTER);
+		
+		scrollBar = new JScrollBar();
+		contentPane.add(scrollBar, BorderLayout.EAST);
+	}
+
+}
