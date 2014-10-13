b7ea9e0cb3c75ce473dc55ff4eef26b4027af1d6
ChristofferNyeland <nyeland94@hotmail.com>
2014-10-13 19:19:16 +0200

    TCP Client added
    TCP client for testing added.
    
    CalenderInfo object added.

---
+++ src/CalendarInfo.java
@@ -0,0 +1,70 @@
+public class CalendarInfo implements java.io.Serializable {
+		private  final long serialVersionUID = 1L;
+		private String overallID = "createCourse";
+		private String activityID;
+		private  String eventID;
+		private  String type;
+		private  String title;
+		private  String description;
+		private  String start;
+		private  String end;
+		private  String location;
+		
+		
+		//Getters and setters for everything, bitch
+		public String getActivityID() {
+			return activityID;
+		}
+		public  void setActivityID(String activityID) {
+			this.activityID = activityID;
+		}
+		public  String getEventID() {
+			return eventID;
+		}
+		public  void setEventID(String eventID) {
+			this.eventID = eventID;
+		}
+		public  String getType() {
+			return type;
+		}
+		public  void setType(String type) {
+			this.type = type;
+		}
+		public  String getTitle() {
+			return title;
+		}
+		public  void setTitle(String title) {
+			this.title = title;
+		}
+		public  String getDescription() {
+			return description;
+		}
+		public  void setDescription(String description) {
+			this.description = description;
+		}
+		public  String getStart() {
+			return start;
+		}
+		public  void setStart(String start) {
+			this.start = start;
+		}
+		public  String getEnd() {
+			return end;
+		}
+		public  void setEnd(String end) {
+			this.end = end;
+		}
+		public  String getLocation() {
+			return location;
+		}
+		public  void setLocation(String location) {
+		this.location = location;
+		}
+		public String getOverallID() {
+			return overallID;
+		}
+		public void setOverallID(String overallID) {
+			this.overallID = overallID;
+		}
+}
+
