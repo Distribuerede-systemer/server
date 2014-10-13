0ddcb38cf3531ecc2e404bea8d239a2ae756beda
kasparses <familien.bjerring@hotmail.com>
2014-10-13 19:45:58 +0200

    fsgdfn

---
+++ src/GUI/LoginInfo.java
@@ -0,0 +1,9 @@
+package GUI;
+
+public class LoginInfo {
+	public void info(){
+		String username = "hej";
+		String password = "nej";
+	}
+
+}
---
+++ src/GUI/UserInformation.java
@@ -0,0 +1,52 @@
+package GUI;
+
+// Fig. 28.30: Person.java
+// Person class that represents an entry in an address book.
+public class UserInformation
+{
+  
+   private String userName;
+   private String password;
+  
+
+   // no-argument constructor
+   public UserInformation()
+   {
+   } // end no-argument Person constructor
+
+   // constructor
+   public UserInformation(  String user, String pass )
+   {
+	   
+//      setUserid(id);
+     
+      setUserName ( user );
+      setPassword ( pass );
+      
+
+   } // end five-argument Person constructor 
+
+
+
+
+public String getUserName() {
+	return userName;
+}
+
+public void setUserName(String user) {
+	userName = user;
+}
+
+public String getPassword() {
+	return password;
+}
+
+public void setPassword(String pass) {
+	password = pass;
+}
+
+}
+
+
+
+ 
