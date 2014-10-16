ad5c3fad50cdec9baae2a4c5bc8d4777f30d31f3
Risgn94 <asger.thyregod@gmail.com>
2014-10-16 22:34:26 +0200

    TCPClient Formateret

--- src/TCPClient.java
+++ src/TCPClient.java
@@ -5,33 +5,35 @@ import java.net.Socket;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import JsonClasses.*;
-public class TCPClient
-{
-public static void main(String[] args) throws Exception {
-String modifiedSentence;
-Gson gson = new GsonBuilder().create();
-CreateCalender CC = new CreateCalender();
-CC.setCalenderName("Din mors kalender2");
-CC.setPublicOrPrivate(1);
-CC.setUserName("John");
-String gsonString = gson.toJson(CC);
-System.out.println(CC);
-System.out.println(gsonString);
 
-Socket clientSocket = new Socket("localhost", 8888);
-DataOutputStream outToServer = new DataOutputStream(
-clientSocket.getOutputStream());
-byte[] input = gsonString.getBytes();
-byte key = (byte) 3.1470;
-byte[] encrypted = input;
-for (int i=0; i<encrypted.length; i++) encrypted[i] = (byte) (encrypted[i] ^ key);
+public class TCPClient {
+	public static void main(String[] args) throws Exception {
+		String modifiedSentence;
+		Gson gson = new GsonBuilder().create();
+		CreateCalender CC = new CreateCalender();
+		CC.setCalenderName("Din mors kalender2");
+		CC.setPublicOrPrivate(1);
+		CC.setUserName("John");
+		String gsonString = gson.toJson(CC);
+		System.out.println(CC);
+		System.out.println(gsonString);
 
-outToServer.write(encrypted);
-outToServer.flush();
-BufferedReader inFromServer = new BufferedReader(new InputStreamReader(clientSocket.getInputStream())); 
-modifiedSentence = inFromServer.readLine();
-System.out.println(modifiedSentence);
-System.out.println("FROM SERVER: " + modifiedSentence);
-clientSocket.close();
-}
+		Socket clientSocket = new Socket("localhost", 8888);
+		DataOutputStream outToServer = new DataOutputStream(
+				clientSocket.getOutputStream());
+		byte[] input = gsonString.getBytes();
+		byte key = (byte) 3.1470;
+		byte[] encrypted = input;
+		for (int i = 0; i < encrypted.length; i++)
+			encrypted[i] = (byte) (encrypted[i] ^ key);
+
+		outToServer.write(encrypted);
+		outToServer.flush();
+		BufferedReader inFromServer = new BufferedReader(new InputStreamReader(
+				clientSocket.getInputStream()));
+		modifiedSentence = inFromServer.readLine();
+		System.out.println(modifiedSentence);
+		System.out.println("FROM SERVER: " + modifiedSentence);
+		clientSocket.close();
+	}
 }
