c3f18468cca8ffe4d9da0fc816f3f89aec72e5aa
CSvaneborg <c.svaneborg@gmail.com>
2014-10-16 16:43:18 +0200

    QueryBuilder fix og events construction

--- src/model/calendar/Events.java
+++ src/model/calendar/Events.java
@@ -1,7 +1,11 @@
 package model.calendar;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.Arrays;
+
+import model.QueryBuild.QueryBuilder;
 
 /**
  * Created by jesperbruun on 10/10/14.
@@ -11,7 +15,19 @@ public class Events {
     ArrayList<Event> events = new ArrayList<Event>();
 
     public ArrayList<Event> getEvents() {
-        return events;
+        
+    	QueryBuilder qb = new QueryBuilder();
+    	try {
+			ResultSet resultSet = qb.selectFrom("events").all().ExecuteQuery();
+			while (resultSet.next())
+			{
+				events.add(new Event());
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+    	
+    	return events;
     }
 
     public void setEvents(ArrayList<Event> event) {
