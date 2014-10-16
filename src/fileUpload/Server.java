c844b558526b06b7c850f0441d00c81847b5695d
mbmunk <mbauermunk@gmail.com>
2014-10-16 20:30:34 +0200

    s

--- src/fileUpload/Server.java
+++ src/fileUpload/Server.java
@@ -45,9 +45,9 @@ public static void main(String[] args) throws IOException {
 
     try {
     	
-    String navn = "hello";
-    	//config fil hvor alt saadan noget staar
-        fos = new FileOutputStream("res\\Uploads\\"+navn);
+    	String file = (String.valueOf(Math.ceil(Math.random()))); 		
+    	
+        fos = new FileOutputStream("res\\Uploads\\"+file);
        
         
        // ..\\..\\..\\..\\..\\..\\..\\..\\Uploads\\hmmmm 
