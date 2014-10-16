f1c59e61bb49a88cd14a835cd787fe86cc452291
johnljoterud <john.ljoterud@gmail.com>
2014-10-16 10:38:31 +0200

    Notes

--- src/model/CreateNote.java
+++ src/model/CreateNote.java
@@ -1,5 +1,25 @@
 package model;
 
-public class CreateNote {
-
-}
+public class CreateNote{
+	
+	Notes notes;
+	
+		
+	
+	
+	public Model CreateNote (
+			int noteID, 
+			String text, 
+			String dateTime, 
+			String createdBy, 
+			boolean isActive, 
+			int eventID){
+		
+			
+		notes = new Notes(noteID, text, text, text, isActive, noteID);
+		
+	
+		return notes;
+		
+	}
+}+
