2e5ab02a69d594ce73e9ae6a09b6d76619a0958e
Mads Skaaning <mads.skaaning@gmail.com>
2014-10-16 16:35:24 +0200

    Opdateret JSon med krypteringsnøgle
    Nu kører den på SQL - og en krypteringsnøgle.
    Har i nogen keys eller timeout tider de rskal gemmes, så smid dem ind i
    en kommentar! eller tilføj

--- src/model/database/Configurations.java
+++ src/model/database/Configurations.java
@@ -12,6 +12,9 @@ public class Configurations {
 	private String port;
 	private String username;
 	private String password;
+
+	// FFkey is used in Encryption.java 
+	private String ffcryptkey;
 	
 	public String getHost() {
 		return host;
@@ -44,6 +47,16 @@ public class Configurations {
 	public void setPassword(String password) {
 		this.password = password;
 	}
+
+		public String getFfcryptkey() {
+		return ffcryptkey;
+	}
+	
+	public void setFfcryptkey(String ffcryptkey) {
+		this.ffcryptkey = ffcryptkey;
+	}
+
+	// Method to read files from jSON file
     
 	public void ReadFile() {
 		JSONParser jsonParser = new JSONParser();
@@ -54,10 +67,16 @@ public class Configurations {
 			Object obj = jsonParser.parse(json);
 			JSONObject jsonObject = (JSONObject) obj;
 
+
+			// Getting json values for SQL variables
 			setHost((String) jsonObject.get("host"));
 			setPort((String) jsonObject.get("port"));
 			setUsername((String) jsonObject.get("username"));
 			setPassword((String) jsonObject.get("password"));
+
+			// Getting json values for KEY variables
+			setFfcryptkey((String) jsonObject.get("ffcryptkey"));
+
 
 		} catch (ParseException ex) {
 			ex.printStackTrace();
