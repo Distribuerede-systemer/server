071db4e90e267d0ff3359e3d531873b8ca6a64d4
Risgn94 <asger.thyregod@gmail.com>
2014-10-16 17:28:51 +0200

    Små ændringer
    Vi har udkommenteret kryptering fra json filen, da vi ikke lige kunne få
    det til at virke. Vi arbejder videre med en hardcodet key istedet

--- src/encryption.java
+++ src/encryption.java
@@ -7,9 +7,12 @@ public class encryption {
 	{
 		Configurations CF = new Configurations();
 //		Defines the decryption value of the byte
-		String crypKey = CF.getFfcryptkey();
-		double gladKo = Double.parseDouble(crypKey);
-		byte ff = (byte) gladKo;
+		//The 4 lines below needs to work later on, but for now, it will be hardcode
+		//System.out.println(CF.getFfcryptkey());
+		//String crypKey = CF.getFfcryptkey();
+		//System.out.println(crypKey);
+		//double gladKo = Double.parseDouble(crypKey);
+		byte ff = (byte) 3.1470;
 //		Generates for loop containing decryption value
 		for(int i = 0 ; i<b.length ; i++)
 		{
