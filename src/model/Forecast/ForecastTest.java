057d1cb7e3b6a8833438d9a5211b1acc9e69a3b0
Andreas Butze <andreasbutze@gmail.com>
2014-10-16 15:09:54 +0200

    forecast added

---
+++ src/model/Forecast/ForecastTest.java
@@ -0,0 +1,14 @@
+package model.Forecast;
+
+public class ForecastTest {
+
+	// Main metode til at k�re en test af vejrudsigt funktionen
+    public static void main(String[] args) {
+
+        ForecastModel fm = new ForecastModel();
+
+        fm.getForecast();
+
+    }
+
+}
