fa590344e8288f58d80a7386998009ea7dfe78ae
Mikkelec <Mikkelc@hotmail.com>
2014-10-16 10:15:50 +0200

    Note opdatering

--- src/model/Notes.java
+++ src/model/Notes.java
@@ -4,7 +4,7 @@ import java.sql.SQLException;
 
 /*
  * 		Mangler:
- * 			- Identifikation på brugeren der har lavet noten
+ * 			- Identifikation pï¿½ brugeren der har lavet noten
  * 			- Hvem der skal kunne redigere noten
  * 			- Mulighed for at slette / redigere noter
  * 			- Active Status
@@ -19,14 +19,51 @@ public class Notes extends Model {
 	private String dateTime;
 	private String createdBy;
 	private String[] editors;
+	private boolean isActive;
+	private int eventID;
 	
-	public Notes(int noteID, String text, String dateTime) {
+	public Notes(int noteID, String text, String dateTime, String createdBy, boolean isActive, int eventID) {
 		super();
 		this.noteID = noteID;
 		this.text = text;
 		this.dateTime = dateTime;
+		this.createdBy = createdBy;
+		this.isActive = isActive;
+		this.eventID = eventID;
+		
 		
 	}
+
+	
+	public int getEventID() {
+		return eventID;
+	}
+
+
+	public void setEventID(int eventID) {
+		this.eventID = eventID;
+	}
+
+
+	public boolean isActive() {
+		return isActive;
+	}
+
+
+	public void setActive(boolean isActive) {
+		this.isActive = isActive;
+	}
+
+
+	public String getCreatedBy() {
+		return createdBy;
+	}
+
+
+	public void setCreatedBy(String createdBy) {
+		this.createdBy = createdBy;
+	}
+
 
 	public int getNoteID() {
 		return noteID;
