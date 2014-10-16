f4f4a670829095dd40ddc3c6169ba067eafa8ca3
nihabr <nihabr@gmail.com>
2014-10-16 15:53:51 +0200

    Finished notes ! need test

--- src/model/note/Note.java
+++ src/model/note/Note.java
@@ -30,57 +30,18 @@ public class Note extends Model{
 				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
-//			String SqlSave = String.format( ""
-//					+ "INSERT INTO notes"
-//					+ "VALUES 	(eventID = %.0f,"
-//					+ "		createdBy = %s"
-//					+ "		text = %s"
-//					+ "		dateTime = %s"
-//					+ "		isActive = %s)", 
-//					eventID, createdBy, text, dateTime, activeStatus);
-			
-			
-//			notes = new NoteModel(noteID, text, text, text, isActive, noteID);
-			
-			//Insert SQL function
-			//Must create note based on note Object
 		}
-		public void EditNote(int noteID, 
-			String text, 
-			String dateTime, 
-			String createdBy, 
-			boolean isActive, 
-			int eventID){
-			
-			notes = new NoteModel(noteID, text, text, text, isActive, noteID);
-			//Insert Update SQL function
-			//SQL statement must replace the note with equal NoteID with this Note
-			//Potentially add edit stamp ?
-		}
+
 		public void DeleteNote (int noteID) throws SQLException {
 			
-			try {
-				resultSet = qb.selectFrom("notes").where("noteID", "= ", String.valueOf(noteID)).ExecuteQuery();
-			} catch (SQLException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-				while(resultSet.next()){
-					notes = new NoteModel(
-							resultSet.getInt("noteID"), 
-							resultSet.getString("text"), 
-							resultSet.getString("dateTime"), 
-							resultSet.getString("createdBy"), 
-							resultSet.getBoolean("isActive"), 
-							noteID);
+					notes = GetNote(noteID);
 					notes.setActive(false);
-					
+					SaveNote(notes);
 					
 				}
 
 			
-		}
-		public NoteModel GetNote (int noteID){
+		public NoteModel GetNote (int noteID) throws SQLException{
 			
 			try {
 				resultSet = qb.selectFrom("notes").where("noteID", "= ", String.valueOf(noteID)).ExecuteQuery();
@@ -96,10 +57,11 @@ public class Note extends Model{
 							resultSet.getString("createdBy"), 
 							resultSet.getBoolean("isActive"), 
 							noteID);
-					
+				}
 					return notes;
 				
 			
+		
 		}
 		public void SaveNote (NoteModel note){
 			
@@ -113,16 +75,9 @@ public class Note extends Model{
 			int eventID = note.getEventID();
 			int noteID = note.getNoteID();
 			
-			String SqlSave = String.format( ""
-					+ "UPDATE notes"
-					+ "SET 	eventID = %.0f,"
-					+ "		createdBy = %s"
-					+ "		text = %s"
-					+ "		dateTime = %s"
-					+ "		isActive = %s,"
-					+ "WHERE noteID = %s", 
-					eventID, createdBy, text, dateTime, activeStatus, noteID);
-			
-		}
+			String[] fields = {"eventID", "createdBy", "text", "dateTime", "isActive"};
+			String[] values = {String.valueOf(noteID), text, dateTime, createdBy, activeStatus};
+			qb.update("notes", fields, values).all();
 				
+		}
 }
