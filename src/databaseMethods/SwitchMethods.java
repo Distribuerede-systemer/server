7ee1d3952ac14d1dadb977e0b516d707064ef896
Jákup Toftum <Jakuptj@gmail.com>
2014-10-16 19:31:21 +0200

    Rydder op i Asgers kode

--- src/databaseMethods/SwitchMethods.java
+++ src/databaseMethods/SwitchMethods.java
@@ -1,18 +1,21 @@
 package databaseMethods;
 import java.sql.SQLException;
 
+import model.Model;
+import model.QueryBuild.QueryBuilder;
 import DatabaseLogic.*;
 
-public class SwitchMethods
+public class SwitchMethods extends Model
 {
-	DatabaseConnection  DC = new DatabaseConnection();
+	QueryBuilder qb = new QueryBuilder();
+	
 	public String createNewCalender (String userName, String calenderName, int privatePublic) throws SQLException
 	{
 		String stringToBeReturned ="";
-		DC.TestConnection();
-		if(DC.authenticateNewCalender(calenderName) == false)
+		testConnection();
+		if(authenticateNewCalender(calenderName) == false)
 		{
-			DC.createNewCalender(calenderName, userName, privatePublic);
+			addNewCalender(calenderName, userName, privatePublic);
 			stringToBeReturned = "The new calender has been created!";
 		}
 		else
@@ -22,6 +25,30 @@ public class SwitchMethods
 		
 		
 		return stringToBeReturned;
+	}
+	
+	public boolean authenticateNewCalender(String newCalenderName) throws SQLException
+	{
+		getConn();
+		boolean authenticate = false;
+		
+		resultSet= qb.selectFrom("calendar").where("name", "=", "newCalendarName").ExecuteQuery();
+				
+				//("select * from test.calender where Name = '"+newCalenderName+"';");
+		while(resultSet.next())
+		{
+			authenticate = true;
+		}
+		return authenticate;
+	}
+	
+	public void addNewCalender (String newCalenderName, String userName, int publicOrPrivate) throws SQLException
+	{
+		String [] keys = {"Name","active","CreatedBy","PrivatePublic"};
+		String [] values = {newCalenderName,"1",userName, Integer.toString(publicOrPrivate)};
+		qb.update("calendar", keys, values).all().Execute();
+		
+//		doUpdate("insert into test.calender (Name, Active, CreatedBy, PrivatePublic) VALUES ('"+newCalenderName+"', '1', '"+userName+"', '"+publicOrPrivate+"');");
 	}
 	public String deleteCalender (String userName, String calenderName)
 	{
