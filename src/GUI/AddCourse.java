ea7649c846fd6c1bee88ef4e784d98c4dcc724cf
AkanGulsen <akangulsen@hotmail.com>
2014-10-16 13:14:43 +0200

    AddCourseAdjustments

--- src/GUI/AddCourse.java
+++ src/GUI/AddCourse.java
@@ -42,15 +42,15 @@
 			add(lblEvents);
 
 			JButton btnDeleteEvent = new JButton("Delete Course");
-			btnDeleteEvent.setBounds(1115, 672, 213, 41);
+			btnDeleteEvent.setBounds(1088, 665, 213, 41);
 			add(btnDeleteEvent);
 
 			JButton btnCreateEvent = new JButton("Create Course");
-			btnCreateEvent.setBounds(1115, 614, 213, 41);
+			btnCreateEvent.setBounds(1088, 615, 213, 41);
 			add(btnCreateEvent);
 
 			JLabel lblUpcomingEvent = new JLabel("Courses:");
-			lblUpcomingEvent.setBounds(171, 139, 136, 33);
+			lblUpcomingEvent.setBounds(149, 132, 124, 33);
 			add(lblUpcomingEvent);
 
 			
@@ -81,7 +81,7 @@
 					BevelBorder.LOWERED, new Color(0, 0, 205), new Color(255, 255,
 							255), new Color(0, 0, 205), new Color(255, 255, 255)),
 					null));
-			scrollPane.setBounds(270, 177, 841, 536);
+			scrollPane.setBounds(244, 170, 841, 536);
 
 			// Add the scroll pane to this panel.
 			add(scrollPane);
