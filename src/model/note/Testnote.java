b28b7cd6c474e56fa2db5ebc95111c2a7c3f70d8
nihabr <nihabr@gmail.com>
2014-10-20 17:55:15 +0200

    Notetesting

--- src/model/note/Testnote.java
+++ src/model/note/Testnote.java
@@ -5,14 +5,15 @@ import model.*;
 
 public class Testnote {
 	public static void main (String [] args) throws SQLException{
-		int nID = 0;
+		int nID = 3;
 		String text = "penis";
 		String date = "1000-01-01 00:00:00";
 		String cb = "createdBy din mor haha";
-		int ia = 0;
+		int ia = 1;
 		int eID = 11;
 		
 		Note note = new Note();
+		note.CreateNote(nID, text, date, cb, ia, eID);
 		note.DeleteNote(nID);
 	}
 }
